2ead509f33da3d53dcdf8c6d2311273643fbd510
Adam Mwaniki <adamndegwa@protonmail.com>
2024-07-10 22:06:52 +0300

    Completes the lab

--- server/app.py
+++ server/app.py
@@ -28,16 +28,32 @@ class Signup(Resource):
         return user.to_dict(), 201
 
 class CheckSession(Resource):
-    pass
+    def get(self):
+        if not session.get('user_id'):
+            return {}, 204
+        return User.query.filter(User.id == session['user_id']).first().to_dict()
 
 class Login(Resource):
-    pass
+    def post(self):
+        json = request.get_json()
+        user = User.query.filter(User.username == json['username']).first()
+        if not user or not user.authenticate(json['password']):
+            return {}, 401
+        session['user_id'] = user.id
+        return user.to_dict()
 
 class Logout(Resource):
-    pass
+
+    def delete(self):
+        session['page_views'] = None
+        session['user_id'] = None
+        return {}, 204
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(Signup, '/signup', endpoint='signup')
+api.add_resource(Login, '/login', endpoint='login')
+api.add_resource(CheckSession, '/check_session', endpoint='check_session')
+api.add_resource(Logout, '/logout', endpoint='logout')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
